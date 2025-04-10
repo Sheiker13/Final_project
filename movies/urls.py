d940d2772b7e588439589550bbba811c552b669a
Sheiker <onetechala@gmail.com>
2025-04-10 12:27:34 +0500

    Убрал конфликт из movies/urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
-from .views import register
-=======
 from .views import (
     register,
     RegisterView,
     catalog_view,
 )
->>>>>>> e25a141 (Улучшил интерфейс)
 
 urlpatterns = [
     path('accounts/register/', register, name='register'),
